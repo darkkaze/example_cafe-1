0a0df82eda14c25a76d3baa062467e71badd9329
Martin Quinta <martin.quinta@storicard.com>
2023-06-27 10:59:49 -0600

    fix

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from flask import Flask, request, render_template
 import sqlite3
-from db import create_db
+
+from flask import Flask, render_template, request
+
+from db import crear_db
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        telefono = request.form['telefono']
+    if request.method == "POST":
+        telefono = request.form["telefono"]
         guardar_visita(telefono)
         count = contar_visitas(telefono)
-        return render_template('index.html', count=count)
-    return render_template('index.html')
+        return render_template("index.html", count=count)
+    return render_template("index.html")
+
 
 # Guardar visita en la base de datos
 def guardar_visita(telefono):
-    conn = sqlite3.connect('cafeteria.db')
+    conn = sqlite3.connect("cafeteria.db")
     cursor = conn.cursor()
     cursor.execute("INSERT INTO visitas (telefono) VALUES (?)", (telefono,))
     conn.commit()
@@ -23,13 +27,14 @@ def guardar_visita(telefono):
 
 
 def contar_visitas(telefono):
-    conn = sqlite3.connect('cafeteria.db')
+    conn = sqlite3.connect("cafeteria.db")
     cursor = conn.cursor()
     cursor.execute("SELECT COUNT(*) FROM visitas WHERE telefono=?", (telefono,))
     count = cursor.fetchone()[0]
     conn.close()
     return count
 
-if __name__ == '__main__':
-    create_db()
+
+if __name__ == "__main__":
+    crear_db()
     app.run()
